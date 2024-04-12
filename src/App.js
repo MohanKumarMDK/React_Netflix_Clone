0be8f0ddf3ef4258804b793612eaefe84bf70d15
MohanKumarMDK <mohankumarmdk007@gmail.com>
2024-04-12 08:32:11 -0700

    modification

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ function App() {
      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-     <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
      <Row title="Horrer Movies" fetchUrl={requests.fetchHorrorMovies} />
      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
